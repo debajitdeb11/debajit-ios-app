7966fb8a85742029c027d908a0eb34ac32c5b3c9
Debajit Deb <debajit.deb@onsolve.com>
2022-10-22 22:10:13 +0530

    Initial Commit

---
+++ Rich-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rich-App
+//
+//  Created by debajit.deb@onsolve.com on 22/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
